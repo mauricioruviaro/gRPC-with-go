fd9fa4de7f0722c479118ba2ec57c4bdc1ed8a5c
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2022-09-15 12:35:35 -0300

    feat: implementado server stream

--- services/user.go
+++ services/user.go
@@ -3,6 +3,7 @@ package services
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/mauricioruviaro/gRPC-with-go/pb"
 )
@@ -25,9 +26,49 @@ func (*UserService) AddUser(ctx context.Context, req *pb.User) (*pb.User, error)
 	//insert no db
 	fmt.Println(req.Name)
 
-	return &pb.User {
-		Id: "123",
-		Name: req.GetName(),
+	return &pb.User{
+		Id:    "123",
+		Name:  req.GetName(),
 		Email: req.GetEmail(),
 	}, nil
+}
+
+func (*UserService) UserService(req *pb.User, stream pb.UserService_AddUserVerboseServer) error {
+	stream.Send(&pb.UserResultStream{
+		Status: "Init",
+		User:   &pb.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Inserting",
+		User:   &pb.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "User has been inserted",
+		User: &pb.User{
+			Id:    "123",
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Completed",
+		User: &pb.User{
+			Id:    "123",
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	return nil
 }
