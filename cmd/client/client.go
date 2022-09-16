2a37d3266c4231ec5c6dfb91ccbf9bb3bb2d5df6
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2022-09-15 23:36:46 -0300

    feat: implementado client para server stream

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/mauricioruviaro/gRPC-with-go/pb"
@@ -19,7 +20,7 @@ func main() {
 	defer connection.Close()
 
 	client := pb.NewUserServiceClient(connection)
-	AddUser(client)
+	AddUserVerbose(client)
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -36,4 +37,30 @@ func AddUser(client pb.UserServiceClient) {
 	}
 
 	fmt.Println(res)
+}
+
+func AddUserVerbose(client pb.UserServiceClient) {
+	req := &pb.User{
+		Id:    "0",
+		Name:  "Joao",
+		Email: "joao@email.com",
+	}
+
+	responseStream, err := client.AddUserVerbose(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+
+	for {
+		stream, err := responseStream.Recv()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("Could not receive the msg: %v", err)
+		}
+
+		fmt.Println("Status:", stream.Status, " - ", stream.GetUser())
+	}
 }
